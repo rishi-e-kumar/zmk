aeff66d03200bde23860853ca2e53ade30500f95
innovaker <66737976+innovaker@users.noreply.github.com>
2020-11-27 17:44:45 +0000

    fix run-test.sh
    changes:
    - building from inside tests
    - mock shield

--- run-test.sh
+++ run-test.sh
@@ -27,7 +27,7 @@ fi
 testcase="$path"
 echo "Running $testcase:"
 
-west build -d build/$testcase -b native_posix -- -DZMK_CONFIG="$(pwd)/$testcase" > /dev/null 2>&1
+west build -s tests -d build/$testcase -b native_posix -- -DSHIELD=mock -DZMK_CONFIG="$(pwd)/$testcase" > /dev/null 2>&1
 if [ $? -gt 0 ]; then
 	echo "FAIL: $testcase did not build" >> ./build/tests/pass-fail.log
 	exit 1
